87813d899741d72c5a6aac5f759e3e397f9a7541
bkim <126510650+bkim89229@users.noreply.github.com>
2024-01-27 01:54:27 -0500

    Delete temp directory

--- temp/utility.js
+++
@@ -1,9 +0,0 @@
-// main 함수 시작 시간 (함수 밖에서 선언)
-export let startTime = new Date();
-
-//시간측정 함수
-export function checkElapsedTime() {
-  var currentTime = new Date();
-  var elapsedTime = (currentTime - startTime) / 1000; // 밀리초 -> 초 변환
-  return elapsedTime;
-}
